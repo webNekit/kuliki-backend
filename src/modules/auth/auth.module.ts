cfd02b7f74a6a970a55e18d0b3cd454b059f8d29
webNekit <mochalov-hunter@mail.ru>
2026-01-16 12:25:53 +0300

    update import jwt module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,9 +5,22 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, PassportModule, JwtModule.register({})],
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_ACCESS_SECRET') as any,
+        signOptions: {
+          expiresIn: config.get<string>('JWT_ACCESS_EXPIRES_IN') as any,
+        },
+      }),
+    }),
+    PrismaModule,
+    PassportModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
